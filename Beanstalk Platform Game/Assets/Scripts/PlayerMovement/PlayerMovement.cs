eea77bdc35b469aa3bee25c861ff96a05f3e72db
Slowri <71567638+Slowri@users.noreply.github.com>
2023-03-09 22:46:45 +0100

    Shader and movement stuff
    adjutsments to leaf shader
    new movement system

--- Beanstalk Platform Game/Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ Beanstalk Platform Game/Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -1,116 +1,85 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Security;
-using UnityEditor;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement Settings")]
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpForce;
-    [SerializeField] private float acceleration;
-    [SerializeField] private float deacceleration;
-    [SerializeField] private float turnMultiplier;
-
-    public float coyoteTimer;
-    public float coyoteTime;
-    public bool hasJumped;
-
-    public float maxJumpTime = 0.5f; // Maximum time that the player can hold down the jump button
-    public float jumpHeight = 5f; // Height that the player jumps when pressing the jump button
-
-    private float jumpTime; // Time that the player has been holding down the jump button
-
-    [Header("Booleans")]
-    public bool isGrounded;
-
-
-
-
-    [Space(25)]
-
-    //Input
-    public float Horizontal;
-    public float Vertical;
-
-    //If grounded bool
+    [SerializeField] float jumpHeight = 5;
+    [SerializeField] float gravityScale = 1;
 
     public Rigidbody2D rb;
 
+
+    //is grounded
+    public GameObject groundCheck;
+    public bool isGrounded;
+    public LayerMask groundLayer;
+
+
+
+    //Movement
+    public float Horizontal;
+    public float speed;
+    public float acceleration;
+    public float deacceleration;
+    public float maxSpeed = 5f;
+
+
+    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Horizontal = Input.GetAxisRaw("Horizontal");
-        Vertical = Input.GetAxisRaw("Vertical");
 
-        if(!isGrounded && !hasJumped)
+
+
+        //Ground check
+        if(Physics2D.OverlapBox(groundCheck.transform.position, groundCheck.transform.localScale, 0, groundLayer))
         {
-            coyoteTimer += 1 * Time.deltaTime;
+            isGrounded = true;
+        } else
+        {
+            isGrounded = false;
         }
 
-
-        //Acceleration
-        if (Mathf.Sign(rb.velocity.x) == Mathf.Sign(Horizontal)) // If moving in same direction
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x + Horizontal * speed * (Time.deltaTime * acceleration), rb.velocity.y);
+            rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
         }
-        else // If moving in opposite direction
+        if (Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0)
         {
-            rb.velocity = new Vector2(rb.velocity.x + Horizontal * speed * (Time.deltaTime * acceleration * turnMultiplier), rb.velocity.y);
+            rb.AddForce(Vector2.down * jumpHeight * .2f, ForceMode2D.Impulse);
         }
 
-        //Deacceleration
+        //Movement
+
+
+    }
+
+
+    public void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput != 0)
+        {
+            // Apply acceleration
+            rb.velocity += new Vector2(horizontalInput * acceleration * Time.deltaTime, 0);
+
+            // Limit velocity to maximum speed
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y);
+        }
+
         if (Horizontal == 0)
-            rb.velocity = new Vector2(rb.velocity.x * deacceleration, rb.velocity.y);
-
-
-        if (Input.GetButtonDown("Jump"))
         {
-            Jump();
-        }
-
-        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -5, 5), rb.velocity.y);
-    }
-
-    public void Jump()
-    {
-        if (isGrounded || coyoteTimer <= coyoteTime && !hasJumped)
-        {
-            jumpTime = 0f;
-            hasJumped = true;
-
-            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
-        }
-    }
-        
-
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        ContactPoint2D[] contacts = new ContactPoint2D[collision.contactCount];
-        collision.GetContacts(contacts);
-        for (int i = 0; i < contacts.Length; i++)
-        {
-            if (contacts[i].normal.y > .75)
-            {
-                isGrounded = true;
-                coyoteTimer = 0;
-                jumpTime = 0f;
-                hasJumped = false;
-            }
+            rb.velocity = new Vector2(rb.velocity.x * (1 - deacceleration * Time.deltaTime), rb.velocity.y);
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        isGrounded = false;
-    }
 }
