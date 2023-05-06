cebe7f5030226c1abd131a02a61da8ec4b401ef5
Murkorus <tjalfe1708@gmail.com>
2023-05-06 22:51:06 +0200

    Audio Test
    Made a script for dynamic walking sounds

--- Beanstalk Platform Game/Assets/Scripts/Audio/Testing Script/WalkingSound.cs
+++ Beanstalk Platform Game/Assets/Scripts/Audio/Testing Script/WalkingSound.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class WalkingSound : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject controller;
+    public AudioSource walkingSound;
+    public AudioClip grass, stone, corruption, cloud;
+
+    private PlayerAnimationController playerAnimationController;
+    private int layerMask;
+
+
+    private void Awake()
     {
-        
+        layerMask = LayerMask.GetMask("Ground");
+        playerAnimationController = controller.GetComponent<PlayerAnimationController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (playerAnimationController.isGrounded == true && playerAnimationController.isMoving == true)  
+        {
+            RaycastHit2D hit = Physics2D.Raycast(controller.transform.position, Vector2.down,Mathf.Infinity,layerMask);
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.CompareTag("Grass"))
+                {
+                    walkingSound.clip = grass;
+                    walkingSound.enabled = true;
+                }
+                else if (hit.collider.gameObject.CompareTag("Stone"))
+                {
+                    walkingSound.clip = stone;
+                    walkingSound.enabled = true;
+                }
+                else if (hit.collider.gameObject.CompareTag("Corruption"))
+                {
+                    walkingSound.clip = corruption;
+                    walkingSound.enabled = true;
+                }
+                else if (hit.collider.gameObject.CompareTag("Cloud"))
+                {
+                    walkingSound.clip = cloud;
+                    walkingSound.enabled = true;
+                }
+            }
+        }
+        else
+        {
+            walkingSound.enabled = false;
+        }
     }
 }
